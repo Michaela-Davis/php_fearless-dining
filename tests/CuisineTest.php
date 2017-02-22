c2446bc104c2c0fbe800bebf0a53d3eb0dd590d4
Erica Wright and Michaela Davis <erica@>
2017-02-22 10:18:22 -0800

    initial test files setup

---
+++ tests/CuisineTest.php
@@ -0,0 +1,28 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Cuisine.php";
+    require_once "src/Restaurant.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=fearless_dining_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CuisineTest extends PHPUnit_Framework_TestCase
+    {
+
+
+
+
+        
+    }
+
+
+
+
+
+?>
---
+++ tests/RestaurantTest.php
@@ -0,0 +1,30 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Cuisine.php";
+    require_once "src/Restaurant.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=fearless_dining_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class RestaurantTest extends PHPUnit_Framework_TestCase
+    {
+
+
+
+
+
+
+    }
+
+
+
+
+
+?>
