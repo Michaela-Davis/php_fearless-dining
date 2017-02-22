eb8e32085cab7ed0d118d04fc081f1fb2fa5606b
Erica Wright and Michaela Davis <michaela@>
2017-02-22 10:40:25 -0800

    create tests for save and getAll functions for Restaurant and Cuisine - fail first test

--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -15,10 +15,20 @@
     class CuisineTest extends PHPUnit_Framework_TestCase
     {
 
+        function test_saveAndGetAll()
+        {
+            $cuisine_name = "Mexican";
+            $new_cuisine = new Cuisine($cuisine_name);
+            $new_cuisine->save();
 
+            $cuisine_name2 = "Thai";
+            $new_cuisine2 = new Cuisine($cuisine_name2);
+            $new_cuisine2->save();
 
+            $result = Cuisine::getAll();
 
-        
+            $this->assertEquals([$new_cuisine, $new_cuisine2], $result);
+        }
     }
 
 
