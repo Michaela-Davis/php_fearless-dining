ed4282c693f15cc6fe387c76ae80979a8e64258c
Erica Wright and Michaela Davis <michaela@>
2017-02-22 11:41:31 -0800

    fail fourth test to findRestaurant

--- tests/RestaurantTest.php
+++ tests/RestaurantTest.php
@@ -43,6 +43,31 @@
             $this->assertEquals([$new_restaurant, $new_restaurant2], $result);
         }
 
+        function test_findRestaurant()
+        {
+            $restaurant_name = "Matador";
+            $address = "1234 N Peach Lane, Portland OR";
+            $keywords = "yummy, cheap, spicy";
+            $cuisine_id = 1;
+            $new_restaurant = new Restaurant($restaurant_name, $address, $keywords, $cuisine_id);
+            $new_restaurant->save();
+
+            $restaurant_name2 = "Sivalai Thai";
+            $address2 = "5678 W Bark Road, Portland OR";
+            $keywords2 = "pricy, friendly";
+            $cuisine_id2 = 2;
+            $new_restaurant2 = new Restaurant($restaurant_name2, $address2, $keywords2, $cuisine_id2);
+            $new_restaurant2->save();
+
+            /// Act   ///
+            $result = Restaurant::findRestaurant($new_restaurant->getId());
+
+            /// Assert ///
+            $this->assertEquals($new_restaurant, $result);
+        }
+
+
+
 
 
 
